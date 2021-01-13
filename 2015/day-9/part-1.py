6138525233500b72b265ca2bd1ebc11bc7a78e1a
Chris Milson <chris.w.milson@gmail.com>
2021-01-13 14:22:07 +0900

    Implement 2015 day-9

---
+++ 2015/day-9/part-1.py
@@ -0,0 +1,54 @@
+from collections import defaultdict
+from typing import List, Tuple
+import re
+
+
+def shortestRoundTrip(distances: List[Tuple[str, str, int]]) -> int:
+    """
+    Calculates the shortest round trip through the graph represented by
+    distances.
+    """
+    graph = defaultdict(dict)
+    for a, b, dist in distances:
+        graph[a][b] = dist
+        graph[b][a] = dist
+
+    been = {place: False for place in graph}
+    result = float("inf")
+
+    def bt(dist, prev):
+        nonlocal result
+        if dist > result:
+            return
+        if all(been.values()):
+            result = dist
+            return
+
+        for place in graph:
+            if been[place]:
+                continue
+            been[place] = True
+            bt(dist + graph[prev][place], place)
+            been[place] = False
+
+    for place in graph:
+        been[place] = True
+        bt(0, place)
+        been[place] = False
+
+    return result
+
+
+distanceRegex = re.compile(r"^(\w+) to (\w+) = (\d+)$")
+
+if __name__ == "__main__":
+    distances = []
+
+    with open('./distances.txt') as f:
+        for line in f:
+            if (match := distanceRegex.match(line)):
+                a, b, dist = match.groups()
+                distances.append((a, b, int(dist)))
+
+    result = shortestRoundTrip(distances)
+    print(result)
