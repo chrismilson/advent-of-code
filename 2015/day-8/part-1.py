84385d50ad3e969c62e12e84538dba43da13041b
Chris Milson <chris.w.milson@gmail.com>
2021-01-13 13:13:04 +0900

    Implement 2015 day-8

---
+++ 2015/day-8/part-1.py
@@ -0,0 +1,19 @@
+def discrepancy(s: str) -> int:
+    """
+    Calculates the difference in the number of characters in the code, and the
+    number of characters in memory; how many extra characters are needed to
+    represent the string in code as opposed to the real string that the code
+    represents.
+    """
+
+    return len(s) - len(eval(s))
+
+
+if __name__ == "__main__":
+    strings = []
+    with open('./list.txt') as f:
+        for line in f:
+            strings.append(line.strip())
+
+    result = sum(discrepancy(s) for s in strings)
+    print(result)
